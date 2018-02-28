a19f6f33065c550e44dbac27f0e980e3aa4f8268
Monica Salama <monicasalama6@gmail.com>
2018-03-01 00:11:16 +0200

    .

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ function initMap() {
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 48.856614, lng: 2.352222},
-    zoom: 13,
+    zoom: 20,
     mapTypeControl: false
   });
 }
@@ -139,7 +139,7 @@ function getWiki(favItemTitle, infoWindow) {
         }
 
         $wikiElem.append('<h3>' + favItemTitle + ' Wikipedia Links </h3>');
-        $wikiElem.append('<ul>');
+        $wikiElem.append('<ul class="list-unstyled components">');
         var articleList = response[1];
         for (var i = 0; i < articleList.length; i++) {
           articleStr = articleList[i];
