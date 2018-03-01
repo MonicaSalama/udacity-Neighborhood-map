5950a59bd17b658babb82897ea1094d1fda7053f
Monica Salama <monicasalama6@gmail.com>
2018-03-01 17:01:34 +0200

    Version 1 completed.

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# udacity-Neighborhood-map
-https://bootstrapious.com/p/bootstrap-sidebar
+# Neighborhood Map
+
+A single page application featuring the map of Paris.
+The application includes some of my favorite locations.
+Each location has a clickable marker to show info about it ex: StreetView, Wikipedia Links related to it.
+You may also filter out of my favorite locations or click on any of them from the sidebar to see it corresponding location on the Map.
+The Application is also responsive, you can view Paris on you preferred device.
+
+
+## Getting Started
+
+### Clone the repository:
+git clone https://github.com/MonicaSalama/udacity-Neighborhood-map
+Open index.html using your favorite Internet browser.
+
+
+### Prerequisites
+
+Internet Browser, ex: Chrome, Firefox.
+
+
+## Sample Run
+
+![Alt text](SampleRun/1.png?raw=true)
+![Alt text](SampleRun/2.png?raw=true)
+![Alt text](SampleRun/2.png?raw=true)
+
+
+## Built With
+
+* [Knockout](http://knockoutjs.com/) - Organization Library
+* [JQuery](https://jquery.com/) - Dependency Management
+* Javascript
+
+
+## Refrences
+
+* [Google Maps ](https://developers.google.com/maps/documentation/)
+* [Google Maps StreetView service](https://developers.google.com/maps/documentation/javascript/streetview)
+* [Google API Udacity repository](https://github.com/udacity/ud864)
+* [MediaWikiAPI for Wikipedia]('https://www.mediawiki.org/wiki/API:Main_page')
+* [knockout]('http://knockoutjs.com/documentation/introduction.html')
+* [bootstrap Sidebar Tutorial](https://bootstrapious.com/p/bootstrap-sidebar)
--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
+// Represents the app viewModel.
+var model;
+// If same marker clicked more than once, if error has occured reload else do nothing
+var errorWiki = false;
+var errorStreetView = false;
+
+// The map is loaded only once and kept in the map variable.
 var map;
-// Load the map.
 // These are the favourite location listings that will be shown to the user.
 var locations = [
   {title: 'Champs-Élysées', location: {lat: 48.865784, lng: 2.307314}},
@@ -15,6 +21,7 @@ var defaultIcon;
 // or clicks an item from the listing
 var highlightedIcon;
 
+// Load the map.
 // Initialize map to Paris
 function initMap() {
   // Constructor creates a new map - only center and zoom are required.
@@ -25,11 +32,10 @@ function initMap() {
   });
 }
 
-// A helper function to great a marker for a specific location
+// A helper function to create a marker for a specific location
 function marker(location, largeInfowindow) {
   var position = location.location;
   var title = location.title;
-  // Create a marker per location, and put into markers array.
    var marker = new google.maps.Marker({
     map: map,
     icon: defaultIcon,
@@ -37,72 +43,15 @@ function marker(location, largeInfowindow) {
     title: title,
     animation: google.maps.Animation.DROP
   });
-
   // Create an onclick event to open an infowindow at each marker.
   marker.addListener('click', function() {
-    populateInfoWindow(this, largeInfowindow);
+    // Populates info window and show wiki articles related to this marker
+    showListingDetails(marker, largeInfowindow);
   });
 
   return marker;
 }
 
-function clearWikipediaLinks() {
-  $('#wikipedia-links').html('');
-}
-// This function populates the infowindow when the marker is clicked. We'll only allow
-// one infowindow which will open at the marker that is clicked, and populate based
-// on that markers position.
-function populateInfoWindow(marker, infowindow) {
-  // Check to make sure the infowindow is not already opened on this marker.
-  if (infowindow.marker != marker) {
-    if(infowindow.marker != null) {
-      clearWikipediaLinks();
-      infowindow.marker.setIcon(defaultIcon);
-    }
-    marker.setIcon(highlightedIcon);
-    infowindow.marker = marker;
-    infowindow.setContent('<div>' + marker.title + '</div>' + '<div id="pano"></div>');
-    // Make sure the marker property is cleared if the infowindow is closed.
-    infowindow.addListener('closeclick', function() {
-      if(infowindow.marker != null) {
-        clearWikipediaLinks();
-        infowindow.marker.setIcon(defaultIcon);
-      }
-      infowindow.marker = null;
-    });
-
-    getWiki(marker.title, infowindow);
-    var streetViewService = new google.maps.StreetViewService();
-    var radius = 50;
-    // In case the status is OK, which means the pano was found, compute the
-    // position of the streetview image, then calculate the heading, then get a
-    // panorama from that and set the options
-    function getStreetView(data, status) {
-      if (status == google.maps.StreetViewStatus.OK) {
-        var nearStreetViewLocation = data.location.latLng;
-        var heading = google.maps.geometry.spherical.computeHeading(
-          nearStreetViewLocation, marker.position);
-          var panoramaOptions = {
-            position: nearStreetViewLocation,
-            pov: {
-              heading: heading,
-              pitch: 30
-            }
-          };
-        var panorama = new google.maps.StreetViewPanorama(
-          document.getElementById('pano'), panoramaOptions);
-      } else {
-          innerHtml += ('<div>No Street View Found</div>');
-          infowindow.setContent(innerHtml);
-      }
-    }
-    // Use streetview service to get the closest streetview image within
-    // 50 meters of the markers position
-    streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-    // Open the infowindow on the correct marker.
-    infowindow.open(map, marker);
-  }
-}
 // This function takes in a COLOR, and then creates a new marker
 // icon of that color. The icon will be 21 px wide by 34 high, have an origin
 // of 0, 0 and be anchored at 10, 34).
@@ -117,7 +66,55 @@ function makeMarkerIcon(markerColor) {
   return markerImage;
 }
 
-// This function retrieves articles related to specific location
+// Populates info window and show wiki articles related to a marker.
+// this function is executed when a marker or an item from the listing is clicked
+function showListingDetails(marker, infowindow) {
+  // Check to make sure the infowindow is not already opened on this marker
+  if (infowindow.marker != marker) {
+    populateInfoWindow(marker, infowindow);
+    getWiki(marker.title, infowindow);
+    return;
+  }
+  // same marker previously cicked but an error has occured.
+  if (errorStreetView) {
+    populateInfoWindow(marker, infowindow);
+  }
+  if (errorWiki) {
+    getWiki(marker.title, infowindow);
+  }
+}
+
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+  if(infowindow.marker != null) {
+    // When user clicks another marker without closing infoWindow of another
+    // previously clicked
+    // then clear the previous marker wiki links + set its icon to default
+    clearWikipediaLinks();
+    infowindow.marker.setIcon(defaultIcon);
+  }
+  // set the marker icon to highlighted
+  marker.setIcon(highlightedIcon);
+  infowindow.marker = marker;
+  infowindow.setContent('<div>' + marker.title + '</div>' + '<div id="pano"></div>');
+  // Make sure the marker property is cleared if the infowindow is closed.
+  infowindow.addListener('closeclick', function() {
+    if(infowindow.marker != null) {
+      clearWikipediaLinks();
+      infowindow.marker.setIcon(defaultIcon);
+    }
+    infowindow.marker = null;
+  });
+
+  // show street view of the marker location in the infoWindow
+  showStreetView(marker, infowindow);
+  // Open the infowindow on the correct marker.
+  infowindow.open(map, marker);
+}
+
+// This function retrieves articles related to specific location * marker location*
 function getWiki(favItemTitle, infoWindow) {
   var wikiurl = 'https://en.wikipedia.org/w/api.php?'
       +'action=opensearch&search=' + favItemTitle
@@ -125,64 +122,124 @@ function getWiki(favItemTitle, infoWindow) {
 
     var $wikiElem = $('#wikipedia-links');
 
+    // if request fails show a message to the user in the wikipedia section.
     var wikiRequestTimeout = setTimeout(function() {
-      $wikiElem.text("failed to get wikipedia resources");
-    }, 8000);
+      errorWiki = true;
+      $wikiElem.text("   Failed to get wikipedia resources   ");
+    }, 4000);
 
     $.ajax({
       url: wikiurl,
       dataType: "jsonp",
       success: function(response) {
-        if (infoWindow == null) {
-          return;
-        }
-
-        var html = ('<ul>');
         var articleList = response[1];
         for (var i = 0; i < articleList.length; i++) {
           articleStr = articleList[i];
           var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-          html += ('<li><a href="' + url +'">'
-              + articleStr + '</a></li>');
+          model.wikiLinksList.push({url: url, title: articleStr});
         };
-        html += ('</ul>');
         if (articleList.length == 0) {
           $wikiElem.text("No wikipedia articles found.")
-        } else {
-          $wikiElem.append(html);
         }
+        // The request succeded timeout must be cleared.
+        errorWiki = false;
         clearTimeout(wikiRequestTimeout)
       }
     });
 }
 
+// clear all data related to a previously clicked marker.
+function clearWikipediaLinks() {
+  // This is important, if a previous request has failes or found
+  // no articles the wiki element will have some indicatif text in it
+  // and must be cleared.
+  $('#wikipedia-links').text('');
+  // remove all previous links related to another previously
+  // clicked marker
+  model.wikiLinksList.removeAll();
+}
 
+// Retrieves and shows streetView of a location in infoWindow.
+function showStreetView(marker, infowindow) {
+  var streetViewService = new google.maps.StreetViewService();
+  var radius = 50;
+  // In case the status is OK, which means the pano was found, compute the
+  // position of the streetview image, then calculate the heading, then get a
+  // panorama from that and set the options
+  function getStreetView(data, status) {
+    if (status == google.maps.StreetViewStatus.OK) {
+      errorStreetView = false;
+      var nearStreetViewLocation = data.location.latLng;
+      var heading = google.maps.geometry.spherical.computeHeading(
+        nearStreetViewLocation, marker.position);
+        var panoramaOptions = {
+          position: nearStreetViewLocation,
+          pov: {
+            heading: heading,
+            pitch: 30
+          }
+        };
+      var panorama = new google.maps.StreetViewPanorama(
+        document.getElementById('pano'), panoramaOptions);
+    } else {
+      errorStreetView = true;
+      $('#pano').text('No Street View Found');
+    }
+  }
+  // Use streetview service to get the closest streetview image within
+  // 50 meters of the markers position
+  streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+}
+
+// Application viewModel
 var ViewModel = function () {
     var self = this;
+    // A list that will hold markers for our favorite locations.
     self.markerList = [];
+    // A list of listings of titles of our favorite locations.
+    // must be kept in sync with markerList.
     self.favItems = ko.observableArray([]);
+    // A list of wikiLinks that get filled when a location is chosen
+    // from the listing or by clicking on a marker.
+    self.wikiLinksList = ko.observableArray([]);
+    // Window shows info about a particular location.
     self.largeInfowindow = new google.maps.InfoWindow();
+    // Used to set the bounds of the map to fit all our favorite locations Markers.
     var bounds = new google.maps.LatLngBounds();
+    // For each location create a marker and add it to favItems list.
     locations.forEach(function(location) {
         self.markerList.push(new marker(location, self.largeInfowindow));
+        // extend bounds to include all markers.
         bounds.extend(location.location);
         self.favItems.push(location.title);
     });
+    // fit map to include all markers.
     map.fitBounds(bounds);
+    // Used to filter location, its value is provided by the user.
     self.filter = ko.observable('');
 
+    // Used to filter markers and favItems list by location name.
+    // filtering is case insensetive.
     self.filterItems = function() {
+      // filter provided by the user.
       var filterText = self.filter().toUpperCase();
+
       self.markerList.forEach(function(marker) {
+
         title = marker.title.toUpperCase();
+        // get marker corresponding item in favorite items list.
         favItemIndex = self.favItems.indexOf(marker.title);
         if (title.indexOf(filterText) > -1) {
+          // Item should be included
           marker.setMap(map);
+          // If it's not in the favItems list add it.
           if(favItemIndex < 0) {
             self.favItems.push(marker.title);
           }
         } else {
+          // Item should be exclueded
           marker.setMap(null);
+          // If  it's in the favItem list remove it.
           if (favItemIndex > -1) {
             self.favItems.remove(marker.title);
           }
@@ -190,23 +247,26 @@ var ViewModel = function () {
       });
     };
 
+    // When an item from the listing is clicked, find its corresponding
+    // marker and show info related to that marker.
     self.clickFavItem = function(favItemTitle) {
       for (var i = 0; i < self.markerList.length; i++) {
         if (favItemTitle == self.markerList[i].title)  {
-          populateInfoWindow(self.markerList[i], self.largeInfowindow);
+          showListingDetails(self.markerList[i], self.largeInfowindow);
         }
       }
     };
 }
 
+// Initialize App
 function initApp() {
+  // Init map
   initMap();
-  // Style the markers a bit. This will be our listing marker icon.
+  // default marker icon.
   defaultIcon = makeMarkerIcon('0091ff');
   // Create a "highlighted location" marker color for when the user
   // mouses over the marker.
   highlightedIcon = makeMarkerIcon('FFFF24');
-  var model = new ViewModel();
+  model = new ViewModel();
   ko.applyBindings(model);
-
 }
